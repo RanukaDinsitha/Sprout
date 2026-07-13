b06479541eedd6931b0825ff3fa283b5a567c72e
Ranuka Dinsitha <ranuka@outlook.co.nz>
2026-07-13 17:13:13 +1200

    Switched yolo11n-cls for yolo26n-cls, while also retraining AI due to incomplete training previously.

--- data/yolo/train.py
+++ data/yolo/train.py
@@ -3,7 +3,7 @@ import os
 
 def main():
     # 1. Load YOLO11 Nano Classification model
-    model = YOLO("yolo11n-cls.pt") 
+    model = YOLO("yolo26n-cls.pt") 
 
     # 2. Train Classification Model
     results = model.train(
@@ -19,8 +19,8 @@ def main():
         device="cpu"           # Set to "cpu" for CPU execution
     )
 
-    print("\n--- Classification Training Complete ---")
-    print(f"Weights saved to: {os.path.abspath('model/sprout_cls/weights/best.pt')}")
+    print("\nThe training of the model has been completed successfully.")
+    print(f"Weights have been saved to: {os.path.abspath('model/sprout_cls/weights/best.pt')}")
 
 if __name__ == "__main__":
     main()
