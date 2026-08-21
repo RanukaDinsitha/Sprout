41dc4a6d787d8f8dd76e5d8a74b1419012fc96d0
Ranuka Dinsitha <ranuka@outlook.co.nz>
2026-08-21 12:05:34 +1200

    Added quantisizing script and a quantisized model file for PythonAnywhere deployment

---
+++ data/yolo/quantisize.py
@@ -0,0 +1,8 @@
+import onnx
+from onnxconverter_common import float16
+
+model = onnx.load("sprout.onnx")
+
+model_fp16 = float16.convert_float_to_float16(model)
+
+onnx.save(model_fp16, "sprout_fp16.onnx")
